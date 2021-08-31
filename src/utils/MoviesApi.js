25eae018943c298470bfdbda9dea8db6d2a65e11
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-08-31 12:27:58 +0300

    feat

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,37 +1,35 @@
 export class MoviesApi {
-    constructor({address, headers}) {
-        this._address = address;
-        this._headers= headers;
-    }
-  
-    _getResponseData(response) {
-      return response.then((res) => {
-        if (res.ok) {
-          return res.json()
-        }
-        return Promise.reject(
-          new Error(`Ошибка получения данных: ${res.status} ${res.statusText}`)
-        )
-      })
-    }
-  
-    getMovies() {
-      return fetch(`${this._address}`, {
-          headers: this._headers,
-        })
-        .then(this._getResponseData)
-        .then ((res) => {
-          return res
-        }
-        )
-    }
+  constructor(options) {
+    this._baseUrl = options.baseUrl
+    this._headers = options.headers
   }
-  
-  const moviesApi = new MoviesApi({
-    address: 'https://api.nomoreparties.co/beatfilm-movies',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  
-  export default moviesApi
+
+  _getResponseData(response) {
+    return response.then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      return Promise.reject(
+        new Error(`Ошибка получения данных: ${res.status} ${res.statusText}`)
+      )
+    })
+  }
+
+  getMovies() {
+    return this._getResponseData(
+      fetch(`${this._baseUrl}`, {
+        method: 'GET',
+        headers: this._headers,
+      })
+    )
+  }
+}
+
+const moviesApi = new MoviesApi({
+  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default moviesApi
